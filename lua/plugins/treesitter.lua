a2ac887aedf4609d2a9880188ca467883e9514f9
René Wirnata <rene.wirnata@pandascience.net>
2022-06-14 17:19:08 +0200

    plug(ts): convert indents to tabs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,15 +3,15 @@ vim.cmd[[set foldexpr=nvim_treesitter#foldexpr()]]
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.zimbu = {
-  install_info = {
-    url = "https://github.com/MichaHoffmann/tree-sitter-hcl", -- local path or git repo
-    files = {"src/parser.c"},
-    -- optional entries:
-    branch = "main", -- default branch in case of git repo if different from master
-    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-  },
-  filetype = "tf", -- if filetype does not match the parser name
+	install_info = {
+		url = "https://github.com/MichaHoffmann/tree-sitter-hcl", -- local path or git repo
+		files = {"src/parser.c"},
+		-- optional entries:
+		branch = "main", -- default branch in case of git repo if different from master
+		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+		requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+	},
+	filetype = "tf", -- if filetype does not match the parser name
 }
 
 -- extend filetype association list
