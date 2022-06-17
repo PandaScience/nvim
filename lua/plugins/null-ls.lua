dfc89433fb4b47b81b3aa9b3c3ce49e8b13a9273
René Wirnata <rene.wirnata@pandascience.net>
2022-06-17 17:06:13 +0200

    lsp: add null-ls config for prettier
    When activated as LS for a specific filetype (here: html, css, js, etc.),
    null-ls will auto-format files on save with :w but not :wq.

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,12 +1,40 @@
-local nls = require("null-ls")
-local b = nls.builtins
+-- https://github.com/jose-elias-alvarez/null-ls.nvim
+-- NOTE: binaries must be already present on the system!
+
+local f = require("null-ls").builtins.formatting
 
 local sources = {
-	b.formatting.prettier,
-	b.formatting.black.with { extra_args = { "--fast", "--line-length=100" } },
+	-- disabled completely b/c will ask on each file write which LSP to use for formatting
+	-- all filetypes
+	-- f.trim_newlines,
+	-- f.trim_whitespace,  -- replaced by https://github.com/axelf4/vim-strip-trailing-whitespace
+
+	-- python
+	--  replaced by https://github.com/python-lsp/python-lsp-black
+	-- f.black.with { extra_args = { "--fast", "--line-length=100" } },
+
+	-- html, css, js and co.
+	f.prettier.with { extra_args = { "--use-tabs" } },
 }
 
-nls.setup({
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+require("null-ls").setup({
 	sources = sources,
 	debug = false,
+
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.formatting()
+				end,
+			})
+		end
+	end,
 })
