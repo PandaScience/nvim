ab3b2ef2b3ce02ee6828595466bb3c732815a6ff
René Wirnata <rene.wirnata@pandascience.net>
2023-03-10 10:33:57 +0100

    chore: apply proper stylua formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,46 +11,41 @@ require("mason-lspconfig").setup()
 -- autoformat if LSP supports it
 vim.api.nvim_create_autocmd("BufWritePre", {
 	buffer = buffer,
-	callback = function()
-		vim.lsp.buf.format { async = true }
-	end
+	callback = function() vim.lsp.buf.format({ async = true }) end,
 })
-
 
 -- BEGIN suggested configuration
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
 	-- Mappings.
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap=true, silent=true, buffer=bufnr }
-	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wl', function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, bufopts)
-	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format({ async = true }) end, bufopts)
 end
 
 local lsp_flags = {
@@ -60,37 +55,37 @@ local lsp_flags = {
 
 -- END suggested configuration
 
-require('lspconfig')['tsserver'].setup{
+require("lspconfig")["tsserver"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
-}
-require('lspconfig')['marksman'].setup{
+})
+require("lspconfig")["marksman"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
-}
-require('lspconfig')['yamlls'].setup{
+})
+require("lspconfig")["yamlls"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
-}
-
+})
 
 -- KEY MAPS --------------------------------------------------------------------
 local map = vim.keymap.set
-map('n', '<leader>ff', function() vim.lsp.buf.format({ async = true }) end)
-map('v', 'ff', function() vim.lsp.buf.formatexpr() end)
-map('n', '<C-space>', function() vim.diagnostic.open_float({scope='line', border='rounded', focusable=false}) end)
-
+map("n", "<leader>ff", function() vim.lsp.buf.format({ async = true }) end)
+map("v", "ff", function() vim.lsp.buf.formatexpr() end)
+map(
+	"n",
+	"<C-space>",
+	function() vim.diagnostic.open_float({ scope = "line", border = "rounded", focusable = false }) end
+)
 
 -- MISC ------------------------------------------------------------------------
 -- disable inline diagnostics
 -- https://github.com/neovim/nvim-lspconfig/issues/662
 -- https://github.com/neovim/neovim/pull/12655
-vim.diagnostic.config({virtual_text = false})
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-	vim.lsp.diagnostic.on_publish_diagnostics, {
-		virtual_text = false,
-		signs = true,
-		update_in_insert = false,
-		underline = true,
-	}
-)
+vim.diagnostic.config({ virtual_text = false })
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	virtual_text = false,
+	signs = true,
+	update_in_insert = false,
+	underline = true,
+})
