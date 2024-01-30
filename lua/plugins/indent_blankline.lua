1acbb378d0f642b8ccd0930cff1b78cd519830fd
René Wirnata <rene.wirnata@pandascience.net>
2024-01-30 11:26:05 +0100

    plug(ibl): adapt to plugin update

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,46 +1,32 @@
-vim.g.indent_blankline_buftype_exclude = {
-	"terminal",
-	"nofile"
+local highlight = {
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
 }
 
-vim.g.indent_blankline_filetype_exclude = {
-	"help",
-	"startify",
-	"dashboard",
-	"packer",
-	"neogitstatus",
-	"NvimTree",
-	"Trouble",
-	"txt",
-	"magit"
-}
+local hooks = require("ibl.hooks")
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
 
-vim.g.indent_blankline_context_patterns = {
-	-- defaults
-	"class",
-	"^func",
-	"method",
-	"^if",
-	"while",
-	"for",
-	"with",
-	"try",
-	"except",
-	"arguments",
-	"argument_list",
-	"object",
-	"dictionary",
-	"element",
-	"table",
-	"tuple",
-	-- yaml
-	"block",
-}
+require("ibl").setup({
+	indent = {
+		char = "▏",
+		tab_char = "▏",
+	},
+	scope = {
+		highlight = highlight,
+	},
+})
 
-require("indent_blankline").setup {
-	space_char_blankline = " ",
-	show_current_context = true,
-	-- show_current_context_start = true,
-}
-
-vim.keymap.set('n', '<leader>i', '<cmd>IndentBlanklineToggle<CR>')
+vim.keymap.set("n", "<leader>i", "<cmd>IBLToggle<CR>")
