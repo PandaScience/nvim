403e7f0bc68a9d3f18a1ebaa794a1d7d3f94ea0f
René Wirnata <rene.wirnata@pandascience.net>
2024-01-30 11:29:09 +0100

    ts: update config syntax and fix formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
-vim.cmd[[set foldmethod=expr]]
-vim.cmd[[set foldexpr=nvim_treesitter#foldexpr()]]
+vim.cmd([[set foldmethod=expr]])
+vim.cmd([[set foldexpr=nvim_treesitter#foldexpr()]])
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.zimbu = {
 	install_info = {
 		url = "https://github.com/MichaHoffmann/tree-sitter-hcl", -- local path or git repo
-		files = {"src/parser.c"},
+		files = { "src/parser.c" },
 		-- optional entries:
 		branch = "main", -- default branch in case of git repo if different from master
 		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
@@ -15,43 +15,42 @@ parser_config.zimbu = {
 }
 
 -- extend filetype association list
-local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
-ft_to_parser.zsh = "bash"
+vim.treesitter.language.register("bash", "zsh")
 
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup({
 	sync_install = false,
 	ensure_installed = {
 		-- supported languages
-		'bash',
-		'comment',
-		'css',
-		'cuda',
-		'dockerfile',
-		'go',
-		'html',
-		'javascript',
-		'json',
-		'latex',
-		'lua',
-		'make',
-		'markdown',
-		'nix',
-		'python',
-		'query',
-		'regex',
-		'typescript',
-		'vim',
-		'yaml',
+		"bash",
+		"comment",
+		"css",
+		"cuda",
+		"dockerfile",
+		"go",
+		"html",
+		"javascript",
+		"json",
+		"latex",
+		"lua",
+		"make",
+		"markdown",
+		"nix",
+		"python",
+		"query",
+		"regex",
+		"typescript",
+		"vim",
+		"yaml",
 		-- manual
-		'hcl'
+		"hcl",
 	},
 	highlight = {
 		enabled = true,
-		disable = { '' },
+		disable = { "" },
 		additional_vim_regex_highlighting = true,
 	},
 	indent = {
-		enable = true
+		enable = true,
 	},
 
 	-- taken from https://github.com/nvim-treesitter/playground#setup
@@ -61,16 +60,16 @@ require'nvim-treesitter.configs'.setup {
 		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
 		persist_queries = false, -- Whether the query persists across vim sessions
 		keybindings = {
-			toggle_query_editor = 'o',
-			toggle_hl_groups = 'i',
-			toggle_injected_languages = 't',
-			toggle_anonymous_nodes = 'a',
-			toggle_language_display = 'I',
-			focus_language = 'f',
-			unfocus_language = 'F',
-			update = 'R',
-			goto_node = '<cr>',
-			show_help = '?',
-		}
-	}
-}
+			toggle_query_editor = "o",
+			toggle_hl_groups = "i",
+			toggle_injected_languages = "t",
+			toggle_anonymous_nodes = "a",
+			toggle_language_display = "I",
+			focus_language = "f",
+			unfocus_language = "F",
+			update = "R",
+			goto_node = "<cr>",
+			show_help = "?",
+		},
+	},
+})
