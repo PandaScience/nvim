ca68d2784b261a7cad2c93f52721fe7c68382d06
René Wirnata <rene.wirnata@pandascience.net>
2024-02-29 20:24:32 +0100

    fzf: open multiple files in buffer instead of quicklist

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,5 +1,9 @@
-require'fzf-lua'.setup {
-  -- winopts = {
-  --   -- border = 'none'
-  -- }
-}
+require("fzf-lua").setup({
+	-- on multiple selecte files, open all with enter
+	-- https://github.com/ibhagwan/fzf-lua/issues/214
+	files = {
+		actions = {
+			["default"] = require("fzf-lua.actions").file_edit,
+		},
+	},
+})
