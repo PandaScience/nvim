e6dc474fa66f1af063b27a208fa5e91611882c2b
René Wirnata <rene.wirnata@pandascience.net>
2024-06-09 22:34:47 +0200

    plug: convert lualine config to lazy format
    - explicitly added dependencies
    - removed unused options

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,31 +1,27 @@
 -- https://github.com/nvim-lualine/lualine.nvim
 
-local navic = require("nvim-navic")
-
-require("lualine").setup({
-	options = {
-		-- theme = 'sonokai'
-		-- theme = 'sonokai'
-		-- theme = 'onedark'
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+return {
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"SmiteshP/nvim-navic",
 	},
-
-	sections = {
-		lualine_c = {
-			{
-				function() return navic.get_location() end,
-				cond = function() return navic.is_available() end,
+	config = function()
+		local navic = require("nvim-navic")
+		require("lualine").setup({
+			options = {
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 			},
-		},
-	},
-
-	-- tabline = {
-	-- 	lualine_a = {'buffers'},
-	-- 	lualine_b = {''},
-	-- 	lualine_c = {''},
-	-- 	lualine_x = {},
-	-- 	lualine_y = {},
-	-- 	lualine_z = {''}
-	-- },
-})
+			sections = {
+				lualine_c = {
+					{
+						function() return navic.get_location() end,
+						cond = function() return navic.is_available() end,
+					},
+				},
+			},
+		})
+	end,
+}
