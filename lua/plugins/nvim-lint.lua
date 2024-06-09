3d1757cb19ea986563f95e3743abeedb3892b0d4
René Wirnata <rene.wirnata@pandascience.net>
2024-06-09 23:53:23 +0200

    plug: convert nvim-lint config to lazy format
    Further:
    - remove markdownlint since marksman LSP is already installed
    - add shellcheck linter

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,17 +1,31 @@
--- https://github.com/mfussenegger/nvim-lint.git
+-- https://github.com/mfussenegger/nvim-lint
+-- https://github.com/rshkarin/mason-nvim-lint
 
-require('lint').linters_by_ft = {
-	markdown = {'markdownlint',}
+return {
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			-- disable inline diagnostics: https://github.com/mfussenegger/nvim-lint/issues/124
+			vim.diagnostic.config({ virtual_text = false })
+
+			-- run linter on text changes and buffer writes
+			vim.api.nvim_create_autocmd({ "BufWritePost", "TextChanged" }, {
+				pattern = "<buffer>",
+				callback = function() require("lint").try_lint() end,
+				group = vim.api.nvim_create_augroup("LintAfterWrite", { clear = true }),
+			})
+
+			-- linter configs
+			require("lint").linters_by_ft = {
+				sh = { "shellcheck" },
+			}
+		end,
+	},
+	{
+		"rshkarin/mason-nvim-lint",
+		lazy = false,
+		opts = {
+			ensure_installed = { "shellcheck" },
+		},
+	},
 }
-
--- vim.cmd [[au BufWritePost <buffer> lua require('lint').try_lint()]]
-
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-
-local lintAfterWrite = augroup("LintAfterWrite", { clear = true })
-autocmd("BufWritePost", {
-	pattern = "<buffer>",
-	callback = function() require('lint').try_lint() end,
-	group = lintAfterWrite,
-})
