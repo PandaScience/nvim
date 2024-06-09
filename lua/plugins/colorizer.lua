def4c958902faf7862ce849c74d7cdb4e3e15aaa
René Wirnata <rene.wirnata@pandascience.net>
2024-06-10 01:02:57 +0200

    plug: convert colorizer config to lazy format

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,15 +1,20 @@
 -- https://github.com/norcalli/nvim-colorizer.lua
 
--- https://github.com/norcalli/nvim-colorizer.lua/issues/85
-require('colorizer').setup(nil, {
-	RGB      = true;         -- #RGB hex codes
-	RRGGBB   = true;         -- #RRGGBB hex codes
-	names    = false;        -- "Name" codes like Blue
-	RRGGBBAA = true;         -- #RRGGBBAA hex codes
-	rgb_fn   = true;         -- CSS rgb() and rgba() functions
-	hsl_fn   = true;         -- CSS hsl() and hsla() functions
-	css      = false;         -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-	css_fn   = false;         -- Enable all CSS *functions*: rgb_fn, hsl_fn
-	-- Available modes: foreground, background
-	mode     = 'background'; -- Set the display mode.
-})
+return {
+	"norcalli/nvim-colorizer.lua",
+	config = function()
+		-- https://github.com/norcalli/nvim-colorizer.lua/issues/85
+		require("colorizer").setup(nil, {
+			RGB = true, -- #RGB hex codes
+			RRGGBB = true, -- #RRGGBB hex codes
+			names = false, -- "Name" codes like Blue
+			RRGGBBAA = true, -- #RRGGBBAA hex codes
+			rgb_fn = true, -- CSS rgb() and rgba() functions
+			hsl_fn = true, -- CSS hsl() and hsla() functions
+			css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+			css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			-- Available modes: foreground, background
+			mode = "background", -- Set the display mode.
+		})
+	end,
+}
