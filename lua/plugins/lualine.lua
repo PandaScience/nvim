b0beb5011b36ebe1e5b3330102a6c7c99768b246
René Wirnata <rene.wirnata@pandascience.net>
2024-06-10 00:40:04 +0200

    ui: add trouble section to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,7 +8,22 @@ return {
 		"SmiteshP/nvim-navic",
 	},
 	config = function()
+		-- navic integration
 		local navic = require("nvim-navic")
+
+		-- trouble integration
+		local trouble = require("trouble")
+		local symbols = trouble.statusline({
+			mode = "lsp_document_symbols",
+			groups = {},
+			title = false,
+			filter = { range = true },
+			format = "{kind_icon}{symbol.name:Normal}",
+			-- The following line is needed to fix the background color
+			-- Set it to the lualine section you want to use
+			hl_group = "lualine_c_normal",
+		})
+
 		require("lualine").setup({
 			options = {
 				component_separators = { left = "", right = "" },
@@ -16,6 +31,12 @@ return {
 			},
 			sections = {
 				lualine_c = {
+					{
+						symbols.get,
+						symbols.has,
+					},
+				},
+				lualine_d = {
 					{
 						function() return navic.get_location() end,
 						cond = function() return navic.is_available() end,
