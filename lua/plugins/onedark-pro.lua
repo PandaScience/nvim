a2d64ef9329ea12948dba0852580d977a5028008
René Wirnata <rene.wirnata@pandascience.net>
2024-06-09 20:37:58 +0200

    plug: convert onedarkpro config to lazy format
    Further:
    - use high priority as stated in lazy docs
    - enforce early (=non-lazy) loading

--- lua/plugins/onedark-pro.lua
+++ lua/plugins/onedark-pro.lua
@@ -1,53 +1,58 @@
-vim.o.background = "dark"
+-- https://github.com/olimorris/onedarkpro.nvim
 
-local onedarkpro = require("onedarkpro")
+return {
+	"olimorris/onedarkpro.nvim",
+	lazy = false,
+	priority = 1000,
+	init = function ()
+		vim.o.background = "dark"
+		vim.cmd("colorscheme onedark")
+	end,
+	opts = {
+		theme = "onedark", -- onedark | onelight | onedark_vivid
 
-onedarkpro.setup({
-	theme = "onedark", -- onedark | onelight | onedark_vivid
+		colors = {}, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
+		ft_highlights = {}, -- Override default highlight groups for specific filetypes
+		highlights = { -- Override default highlight groups
+			-- better contrast in visual selection
+			Visual = { style = "reverse" },
+			-- better contrast for floating windows
+			FloatBorder = { fg = "${orange}", bg = "NONE" },
+			-- control characters (like ^M)
+			SpecialKey = { fg = "#FF0000", bg = "NONE", style = "bold" },
+			-- listchars (see :h listchars) (leading tab chars not visible when using IndentBlankline)
+			NonText = { link = "Whitespace" }, -- eol, extends, precedes
+			Whitespace = { link = "Whitespace" }, -- nbsp, space, tab, multispace, lead, trail
+			-- plugin override (see :h hl-IndentBlanklineSpaceChar) (tab symbol if no other char comes before)
+			IndentBlanklineSpaceChar = { fg = "#555555", bg = "NONE" }, -- tab and space indents
+			-- make non-active (treesitter) indent lines less visible
+			IndentBlanklineChar = { fg = "#222222", bg = "None" },
+		},
 
-	colors = {}, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
-	ft_highlights = {}, -- Override default highlight groups for specific filetypes
-	highlights = { -- Override default highlight groups
-		-- better contrast in visual selection
-		Visual = { style = "reverse" },
-		-- better contrast for floating windows
-		FloatBorder = { fg = "${orange}", bg = "NONE" },
-		-- control characters (like ^M)
-		SpecialKey = { fg = "#FF0000", bg = "NONE", style = "bold" },
-		-- listchars (see :h listchars) (leading tab chars not visible when using IndentBlankline)
-		NonText = { link = "Whitespace" }, -- eol, extends, precedes
-		Whitespace = { link = "Whitespace" }, -- nbsp, space, tab, multispace, lead, trail
-		-- plugin override (see :h hl-IndentBlanklineSpaceChar) (tab symbol if no other char comes before)
-		IndentBlanklineSpaceChar = { fg = "#555555", bg = "NONE" }, -- tab and space indents
-		-- make non-active (treesitter) indent lines less visible
-		IndentBlanklineChar = { fg = "#222222", bg = "None" },
+		plugins = { -- All plugins are activated by default
+			nvim_lsp = true,
+			polygot = false,
+			treesitter = true,
+		},
+
+		styles = {
+			comments = "italic",
+			functions = "NONE",
+			keywords = "bold,italic",
+			strings = "NONE",
+			variables = "NONE",
+			virtual_text = "NONE",
+		},
+
+		options = {
+			bold = true, -- Use the themes opinionated bold styles?
+			italic = true, -- Use the themes opinionated italic styles?
+			underline = false, -- Use the themes opinionated underline styles?
+			undercurl = true, -- Use the themes opinionated undercurl styles?
+			cursorline = true, -- Use cursorline highlighting?
+			transparency = true, -- Use a transparent background?
+			terminal_colors = false, -- Use the theme's colors for Neovim's :terminal?
+			window_unfocused_color = true, -- When the window is out of focus, change the normal background?
+		},
 	},
-
-	plugins = { -- All plugins are activated by default
-		nvim_lsp = true,
-		polygot = false,
-		treesitter = true,
-	},
-
-	styles = {
-		comments = "italic",
-		functions = "NONE",
-		keywords = "bold,italic",
-		strings = "NONE",
-		variables = "NONE",
-		virtual_text = "NONE",
-	},
-
-	options = {
-		bold = true, -- Use the themes opinionated bold styles?
-		italic = true, -- Use the themes opinionated italic styles?
-		underline = false, -- Use the themes opinionated underline styles?
-		undercurl = true, -- Use the themes opinionated undercurl styles?
-		cursorline = true, -- Use cursorline highlighting?
-		transparency = true, -- Use a transparent background?
-		terminal_colors = false, -- Use the theme's colors for Neovim's :terminal?
-		window_unfocused_color = true, -- When the window is out of focus, change the normal background?
-	},
-})
-
-onedarkpro.load()
+}
