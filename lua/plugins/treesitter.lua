82827cf9a695d9b2bd5d172118fe53ea7b33868f
René Wirnata <rene.wirnata@pandascience.net>
2024-06-09 23:01:44 +0200

    plug: convert treesitter config to lazy format
    - removed deprecated playground config (use :Inspect[Tree]) instead
    - removed local hcl parser since it's now officially supported

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,77 +1,51 @@
-vim.cmd([[set foldmethod=expr]])
-vim.cmd([[set foldexpr=nvim_treesitter#foldexpr()]])
+-- https://github.com/nvim-treesitter/nvim-treesitter
 
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.zimbu = {
-	install_info = {
-		url = "https://github.com/MichaHoffmann/tree-sitter-hcl", -- local path or git repo
-		files = { "src/parser.c" },
-		-- optional entries:
-		branch = "main", -- default branch in case of git repo if different from master
-		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-		requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-	},
-	filetype = "tf", -- if filetype does not match the parser name
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = function() require("nvim-treesitter.install").update({ with_sync = true })() end,
+	init = function()
+		vim.cmd([[set foldmethod=expr]])
+		vim.cmd([[set foldexpr=nvim_treesitter#foldexpr()]])
+	end,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			sync_install = false,
+			ensure_installed = {
+				"bash",
+				"c",
+				"comment",
+				"cpp",
+				"css",
+				"cuda",
+				"dockerfile",
+				"go",
+				"hcl",
+				"html",
+				"javascript",
+				"json",
+				"latex",
+				"lua",
+				"make",
+				"markdown",
+				"nix",
+				"python",
+				"query",
+				"regex",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+			highlight = {
+				enabled = true,
+				disable = { "" },
+				additional_vim_regex_highlighting = true,
+			},
+			indent = {
+				enable = true,
+			},
+		})
+
+		-- extend filetype association list
+		vim.treesitter.language.register("bash", "zsh")
+	end,
 }
-
--- extend filetype association list
-vim.treesitter.language.register("bash", "zsh")
-
-require("nvim-treesitter.configs").setup({
-	sync_install = false,
-	ensure_installed = {
-		-- supported languages
-		"bash",
-		"c",
-		"comment",
-		"cpp",
-		"css",
-		"cuda",
-		"dockerfile",
-		"go",
-		"html",
-		"javascript",
-		"json",
-		"latex",
-		"lua",
-		"make",
-		"markdown",
-		"nix",
-		"python",
-		"query",
-		"regex",
-		"typescript",
-		"vim",
-		"yaml",
-		-- manual
-		"hcl",
-	},
-	highlight = {
-		enabled = true,
-		disable = { "" },
-		additional_vim_regex_highlighting = true,
-	},
-	indent = {
-		enable = true,
-	},
-
-	-- taken from https://github.com/nvim-treesitter/playground#setup
-	playground = {
-		enable = true,
-		disable = {},
-		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-		persist_queries = false, -- Whether the query persists across vim sessions
-		keybindings = {
-			toggle_query_editor = "o",
-			toggle_hl_groups = "i",
-			toggle_injected_languages = "t",
-			toggle_anonymous_nodes = "a",
-			toggle_language_display = "I",
-			focus_language = "f",
-			unfocus_language = "F",
-			update = "R",
-			goto_node = "<cr>",
-			show_help = "?",
-		},
-	},
-})
