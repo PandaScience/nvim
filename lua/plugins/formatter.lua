6ce7c8046d404471c9392ed83bbe428ffe3dbaf5
René Wirnata <rene.wirnata@pandascience.net>
2024-06-11 11:09:36 +0200

    plug: convert formatter config to lazy format
    - disabled lazy loading
    - moved auto command for auto-formatting to `init` function
    - re-ordered some filetype settings
    - added new key binding

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,50 +1,68 @@
 -- https://github.com/mhartington/formatter.nvim
 
-local util = require("formatter.util")
+return {
+	"mhartington/formatter.nvim",
+	lazy = false,
+	init = function()
+		-- https://github.com/mhartington/formatter.nvim?tab=readme-ov-file#beforeafter-format-hooks
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			desc = "Auto format files with `formatter` w/o saving if available",
+			command = "FormatLock",
+			group = vim.api.nvim_create_augroup("autoformatOnSave", { clear = true }),
+		})
+	end,
+	-- BUG: ? https://github.com/mhartington/formatter.nvim/issues/286
+	-- -> use config instead of opts to prevent `missing module` errors b/c of `require` statements
+	config = function()
+		local opts = {
+			-- https://github.com/mhartington/formatter.nvim/tree/master/lua/formatter/filetypes
+			filetype = {
+				lua = {
+					-- https://www.reddit.com/r/neovim/comments/vtdmry/stylua_now_supports_collapsing_simple_statements/
+					function()
+						f = require("formatter.filetypes.lua").stylua()
+						table.insert(f.args, 1, "--collapse-simple-statement='Always'")
+						return f
+					end,
+				},
+				css = {
+					require("formatter.filetypes.css").prettierd,
+				},
+				markdown = {
+					require("formatter.filetypes.markdown").prettierd,
+				},
+				yaml = {
+					require("formatter.filetypes.yaml").prettierd,
+				},
+				json = {
+					require("formatter.filetypes.json").prettierd,
+				},
+				jsonc = {
+					function()
+						local util = require("formatter.util")
+						return {
+							exe = "prettier",
+							args = {
+								"--trailing-comma none",
+								"--stdin-filepath",
+								util.escape_path(util.get_current_buffer_file_path()),
+							},
+							stdin = true,
+						}
+					end,
+				},
+				c = {
+					require("formatter.filetypes.c").clangformat,
+				},
+				["*"] = {
+					require("formatter.filetypes.any").remove_trailing_whitespace,
+				},
+			},
+		}
 
--- https://github.com/mhartington/formatter.nvim/tree/master/lua/formatter/filetypes
-require("formatter").setup({
-	filetype = {
-		lua = {
-			-- https://www.reddit.com/r/neovim/comments/vtdmry/stylua_now_supports_collapsing_simple_statements/
-			function()
-				f = require("formatter.filetypes.lua").stylua()
-				table.insert(f.args, 1, "--collapse-simple-statement='Always'")
-				return f
-			end,
-		},
-		markdown = {
-			require("formatter.filetypes.markdown").prettierd,
-		},
-		yaml = {
-			require("formatter.filetypes.yaml").prettierd,
-		},
-		json = {
-			require("formatter.filetypes.json").prettierd,
-		},
-		jsonc = {
-			function()
-				return {
-					exe = "prettier",
-					args = {
-						"--trailing-comma none",
-						"--stdin-filepath",
-						util.escape_path(util.get_current_buffer_file_path()),
-					},
-					stdin = true,
-				}
-			end,
-		},
-		css = {
-			require("formatter.filetypes.css").prettierd,
-		},
-		c = {
-			require("formatter.filetypes.c").clangformat,
-		},
-		["*"] = {
-			require("formatter.filetypes.any").remove_trailing_whitespace,
-		},
+		require("formatter").setup(opts)
+	end,
+	keys = {
+		{ "<leader>F", "<cmd>Format<cr>", desc = "Format the file using formatter.nvim" },
 	},
-})
-
--- NOTE: autoformat-on-save augroup defined in core/autocmds.lua
+}
