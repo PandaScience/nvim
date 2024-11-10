fcfc62870aefa564f0d54dcbb16f17035d91a543
René Wirnata <rene.wirnata@pandascience.net>
2024-11-10 11:51:37 +0100

    chore: apply proper stylua formatting

--- init.lua
+++ init.lua
@@ -1,15 +1,13 @@
 -- vim: noet ai ci pi sts=0 sw=4 ts=4
 
 local modules = {
-	'core.lazy',
-	'core.settings',
-	'core.mappings',
-	'core.autocmds',
+	"core.lazy",
+	"core.settings",
+	"core.mappings",
+	"core.autocmds",
 }
 
 for _, module in ipairs(modules) do
 	local ok, err = pcall(require, module)
-	if not ok then
-		error("Error loading " .. module .. "\n\n" .. err)
-	end
+	if not ok then error("Error loading " .. module .. "\n\n" .. err) end
 end
