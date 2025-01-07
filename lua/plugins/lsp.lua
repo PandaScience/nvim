756a10e2c806a67eb84a7132126e9d983620dd38
René Wirnata <rene.wirnata@pandascience.net>
2025-01-07 14:20:59 +0100

    lsp: prevent error during lsp-autoformat on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -104,7 +104,8 @@ local config = function()
 	-- autoformat if LSP supports it
 	vim.api.nvim_create_autocmd("BufWritePre", {
 		buffer = buffer,
-		callback = function() vim.lsp.buf.format({ async = true }) end,
+		-- no async here to prevent "buffer is not modifiable" error
+		callback = function() vim.lsp.buf.format({ async = false }) end,
 	})
 
 	-- disable inline diagnostics: https://github.com/neovim/nvim-lspconfig/issues/662
