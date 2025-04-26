f0d3cea6fed3cc2c695b7ffa6bdcd2e2ddbf8210
René Wirnata <rene.wirnata@pandascience.net>
2025-04-26 22:58:43 +0200

    plug: remove sunsetted tabnine basic
    See https://docs.tabnine.com/main/welcome/readme/tabnine-subscription-plans/basic#sunsetting-tabnine-basic

--- lua/plugins/tabnine.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/tzachar/cmp-tabnine
--- TODO: https://github.com/codota/tabnine-nvim
-
-return {
-	"tzachar/cmp-tabnine",
-	build = "./install.sh",
-	dependencies = "hrsh7th/nvim-cmp",
-}
