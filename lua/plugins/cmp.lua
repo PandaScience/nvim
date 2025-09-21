57a40f66762095ce8ca2d0e0b227eeae9767cbf2
René Wirnata <rene.wirnata@pandascience.net>
2025-09-21 10:17:58 +0200

    cmp: add vscode-like pictograms for lsp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,7 +3,10 @@
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
+		-- lsp
 		"neovim/nvim-lspconfig",
+		"onsails/lspkind.nvim",
+		-- sources
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
@@ -12,8 +15,33 @@ return {
 		"hrsh7th/vim-vsnip-integ",
 	},
 	config = function()
+		local lspkind = require("lspkind")
 		local cmp = require("cmp")
+
 		cmp.setup({
+			-- use lspkind for vscode-like pictograms
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = "symbol", -- show only symbol annotations
+					maxwidth = {
+						-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+						-- can also be a function to dynamically calculate max width such as
+						-- menu = function() return math.floor(0.45 * vim.o.columns) end,
+						menu = 50, -- leading text (labelDetails)
+						abbr = 50, -- actual suggestion item
+					},
+					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+					-- The function below will be called before any actual modifications from lspkind
+					-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+					before = function(entry, vim_item)
+						-- ...
+						return vim_item
+					end,
+				}),
+			},
+
 			snippet = {
 				expand = function(args) vim.fn["vsnip#anonymous"](args.body) end,
 			},
