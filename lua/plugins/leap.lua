72da6bd3e9688774dc7e142f6e707001b23a6d89
René Wirnata <rene.wirnata@pandascience.net>
2026-04-03 15:16:23 +0200

    plug: update lazy plugin references

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,7 +1,7 @@
 -- https://github.com/ggandor/leap.nvim
 
 return {
-	"ggandor/leap.nvim",
+	url = "https://codeberg.org/andyg/leap.nvim",
 	-- NOTE: Using the keys feature of lazy.nvim might cause problems (see README)
 	config = function()
 		vim.keymap.set({ "n", "x", "o" }, "l", "<Plug>(leap-forward)")
