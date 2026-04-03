6e1745bfd79de3290efe8248187ae958fe203f98
René Wirnata <rene.wirnata@pandascience.net>
2026-04-03 17:11:25 +0200

    plug: update lazy config

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -11,4 +11,16 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {})
+-- require("lazy").setup("plugins", {})
+require("lazy").setup({
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+	-- disable luarocks
+	rocks = {
+		enabled = false,
+	},
+})
